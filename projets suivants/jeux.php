c0611c3498891686c4dd6051a8386ae3ce23bbef
loewe19 <angembontchou@gmail.com>
2024-04-04 15:46:00 +0200

    version1.0

---
+++ projets suivants/jeux.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta name="author" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<title>Soka push</title>
+<link rel="stylesheet" href="baser.css">
+</head>
+<body>
+<div class="right-align">
+    <nav>
+        <ul>
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">À propos</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+            <form class="d-flex input-group w-auto">
+                <input
+                    type="search"
+                    class="form-control rounded"
+                    placeholder="Rechercher"
+                    aria-label="Rechercher"
+                    aria-describedby="search-addon"
+                />
+                <span class="input-group-text border-0" id="search-addon">
+                    <i class="fas fa-search"></i>
+                </span>
+            </form>
+        </div>
+    </nav>
+</div>
+
+
+</body>
